6dcfa7db643f6903a4a691d873bc2a27fb9e5bf0
Fernando Acevedo Colman <fernandotwe@hotmail.com>
2024-06-13 18:30:19 +0200

    Update for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import React from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fernandotwe.github.io',
   base: '/ip-ai-test',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()],
 });
