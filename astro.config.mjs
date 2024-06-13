e995ad21de49b098903e82ff16c4dfaa21db842e
Fernando Acevedo Colman <fernandotwe@hotmail.com>
2024-06-13 18:14:50 +0200

    Add IP analysis page with consultaId generation and webhook functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import React from '@astrojs/react';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://fernandotwe.github.io',
   base: '/ip-ai-test',
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
 });
