b0ea07f102b8afdc3c4bb0043cc4c59cf76e3fc4
Fernando Acevedo Colman <fernandotwe@hotmail.com>
2024-06-29 11:40:20 +0200

    Nueva versión con resultados CORE

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly PUBLIC_PROCESIO_URL: string;
+    readonly PUBLIC_IPIFY_URL: string;
+    // más variables de entorno...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
